999f2ed2c80dbb78e05c044f235c9bff71130dd1
jerryOluranti <faluyijeremiah2018@gmail.com>
2023-09-25 05:05:30 +0100

    ennhanced model

--- hugging_vit_eval.py
+++ hugging_vit_eval.py
@@ -1,23 +1,31 @@
-from transformers import ViTFeatureExtractor, ViTForImageClassification
+from transformers import ViTFeatureExtractor, ViTImageProcessor, ViTForImageClassification, TrainingArguments
 import numpy as np
 from evaluate import load
+from datasets import load_dataset, load_metric
 
 metric = load("accuracy")
 
 model_path = "./out/GLAUCOMAVIT/20_2023-08-04-11-02-36/"
 
 
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
+def compute_metrics(pred):
+    predictions = np.argmax([pred.detach().numpy() for pred in pred.logits], axis=-1)
+    print(len([1 for p in predictions if p == 1]), len([0 for p in predictions if p == 0]))
+    return metric.compute(predictions=predictions)
 
-tokenizer = ViTFeatureExtractor.from_pretrained(model_path + "feature_extractor")
-model = ViTForImageClassification.from_pretrained(model_path + "model", return_dict=False)
+# tokenizer = ViTFeatureExtractor.from_pretrained(model_path + "feature_extractor")
+tokenizer = ViTImageProcessor.from_pretrained(model_path + "feature_extractor")
+model = ViTForImageClassification.from_pretrained(model_path + "model")
 
-test_image = "./test/refuge_test_4.jpg"
-image = tokenizer(images=test_image, return_tensors="pt")
+test_images = load_dataset("imagefolder", data_dir="./test/")
 
-preds = model(**image)
+image = tokenizer(images=test_images["test"]['image'], return_tensors="pt")
 
-print(compute_metrics(preds))
+# print(model.config)
+
+outputs = model(**image)
+
+# print(outputs)
+
+print(compute_metrics(outputs))
+
